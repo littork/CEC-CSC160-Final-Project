7827a3c2f69ab2beee928af7ccd158d5b11b2c50
Littork <littork@gmail.com>
2019-11-10 12:32:35 -0700

    First attempt at actual auto updater functionality

--- interface/src/Autoupdate.cpp
+++ interface/src/Autoupdate.cpp
@@ -10,10 +10,9 @@
 std::string Autoupdate::buildNumber = "";
 
 void Autoupdate::begin() {
-//#ifdef BUILD_NUMBER
+#ifdef BUILD_NUMBER
 	if (!buildNumber.size()) {
-		buildNumber = std::to_string(21);
-		// buildNumber = std::to_string(BUILD_NUMBER);
+		buildNumber = std::to_string(BUILD_NUMBER);
 	}
 
 	std::string currentBuild = Shell::run("curl -fsSLI -o /dev/null -w %{url_effective} " REPOSITORY_URL);
@@ -22,10 +21,13 @@ void Autoupdate::begin() {
 	if (buildNumber != currentBuild) {
 		nana::msgbox m(NULL, "Update Available", nana::msgbox::yes_no);
 		m << "An update is available. You are currently on build #" << buildNumber << ", but build #" << currentBuild << " has been released. Do you wish to update now?";
-		//if (m() == m.pick_yes) {
+		if (m() == m.pick_yes) {
 			// We want to update
-			
-		//}
+			std::system("curl -L https://github.com/littork/CEC-CSC160-Final-Project/raw/master/interface/autoupdater.exe -o autoupdater.exe");
+			std::string app_path(4096, '\0');
+			app_path.resize(GetModuleFileName(0, &app_path.front(), app_path.size()));
+			std::system(std::string("autoupdater.exe \"" + app_path + "\" https://github.com/littork/CEC-CSC160-Final-Project/releases/download/%23" + buildNumber + "/interface_x64.exe").c_str());
+		}
 	}
-//#endif
+#endif
 }
