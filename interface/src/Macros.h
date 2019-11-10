1591d9933a445c694ebf1026da12aa6c0659868f
Littork <littork@gmail.com>
2019-11-09 20:38:12 -0700

    Display build date

--- interface/src/Macros.h
+++ interface/src/Macros.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define BUILD_DATE __DATE__ " " __TIME__
+
 // Colors
 
 #define COLOR_HEX_DEFAULT 0xf0f0f0
--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -2,16 +2,22 @@
 #include <windows.h>
 
 #include <string>
+#include <regex>
 
 #include "UICore.h"
 
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
 	Form form = Form(500, 300);
 
+	std::string dateTime = " (" BUILD_DATE ")";
+	std::regex e("  ");
+
+	std::string processedDateTime = "";
+	std::regex_replace(std::back_inserter(processedDateTime), dateTime.begin(), dateTime.end(), e, " $2");
 #ifdef BUILD_NUMBER
-	form.setTitle("CEC Final Project - Release Build " + std::to_string(BUILD_NUMBER));
+	form.setTitle("CEC Final Project - Release Build " + std::to_string(BUILD_NUMBER) + processedDateTime);
 #else
-	form.setTitle("CEC Final Project - Dev Build");
+	form.setTitle("CEC Final Project - Dev Build" + processedDateTime);
 #endif
 
 	form.display();
