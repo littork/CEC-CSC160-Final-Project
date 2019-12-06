f87d9b311315292746f17bb167d1f76a47a92cbf
Littork <littork@gmail.com>
2019-12-06 11:52:12 -0700

    Add game launch buttons

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -10,9 +10,79 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/listbox.hpp>
+#include <nana/gui/widgets/textbox.hpp>
 #include <nana/gui/widgets/group.hpp>
 
 #include "Autoupdate.h"
+
+class inline_widget : public nana::listbox::inline_notifier_interface {
+private:
+	virtual void create(nana::window wd) override {
+		/*txt_.create(wd);
+		txt_.events().click([this] {
+			indicator_->selected(pos_);
+		});
+
+		txt_.events().mouse_move([this] {
+			indicator_->hovered(pos_);
+		});
+
+		txt_.events().key_char([this](const nana::arg_keyboard& arg) {
+			if (arg.key == nana::keyboard::enter) {
+				arg.ignore = true;
+				indicator_->modify(pos_, txt_.caption());
+			}
+		});
+
+		txt_.events().text_changed([this]() {
+			indicator_->modify(pos_, txt_.caption());
+		});*/
+
+		btn_.create(wd);
+		btn_.caption("Launch");
+		btn_.events().click([this] {
+			auto& lsbox = dynamic_cast<nana::listbox&>(indicator_->host());
+			lsbox.erase(lsbox.at(pos_));
+		});
+
+		btn_.events().mouse_move([this] {
+			indicator_->hovered(pos_);
+		});
+	}
+
+	virtual void activate(inline_indicator& ind, index_type pos) {
+		indicator_ = &ind;
+		pos_ = pos;
+	}
+
+	void notify_status(status_type status, bool status_on) override {
+		if ((status_type::selecting == status) && status_on)
+			txt_.focus();
+	}
+
+	void resize(const nana::size& dimension) override {
+		auto sz = dimension;
+		sz.width -= (sz.width < 50 ? 0 : 115); //Check for avoiding underflow.
+		txt_.size(sz);
+
+		nana::rectangle r(sz.width + 5, 0, 45, sz.height);
+		btn_.move(r);
+	}
+
+	virtual void set(const value_type& value) {
+		if (txt_.caption() != value)
+			txt_.caption(value);
+	}
+
+	bool whether_to_draw() const override {
+		return false;
+	}
+private:
+	inline_indicator* indicator_ {nullptr};
+	index_type pos_;
+	nana::textbox txt_;
+	nana::button btn_;
+};
 
 int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
 	// Autoupdater
@@ -68,26 +138,32 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLi
 
 	// List of games
 
-	nana::listbox gamesList(form.form, nana::rectangle{ 10, 10, 480, 100 });
+	nana::listbox gamesList(form.form, nana::rectangle{ 5, 5, 490, 290 });
 	gamesList.append_header("Game Name");
 	gamesList.append_header("Game Version");
 	gamesList.append_header("Game Author(s)");
+	gamesList.append_header("Game Launch");
 
 	auto category = gamesList.at(0);
 	gamesList.auto_draw(false);
 	
+	//nana::button launchButton = nana::button(form.form, nana::rectangle(10, 10, 100, 25));
+	//launchButton.caption("Launch");
+
 	// gamesList.at(0).append({ "OpenGL Test", "1.0", "Dylan Pozarnsky" });
-	gamesList.at(0).append({ "Pacman", "1.0", "Alexander Marsh" });
-	gamesList.at(0).append({ "Walking Simulator", "1.0", "Brendan Mccleery" });
+	gamesList.at(0).append({ "Pacman", "1.0", "Alexander Marsh", "" });
+	gamesList.at(0).append({ "Walking Simulator", "1.0", "Brendan Mccleery", "" });
+
+	gamesList.at(0).inline_factory(3, nana::pat::make_factory<inline_widget>());
 
 	gamesList.auto_draw(true);
 
 	// Game details
 
-	nana::group gameDetailsGroup(form.form, nana::rectangle{ 10, 120, 480, 100 });
+	//nana::group gameDetailsGroup(form.form, nana::rectangle{ 10, 120, 480, 100 });
 
-	gameDetailsGroup.caption("Game Details");
-	gameDetailsGroup.bgcolor(NANA_COLOR_PURE_WHITE);
+	//gameDetailsGroup.caption("Game Details");
+	//gameDetailsGroup.bgcolor(NANA_COLOR_PURE_WHITE);
 
 	/*nana::label gameNameLabel(gameDetailsGroup);
 	gameNameLabel.caption("Game Name");
